6ddccdbd4a5586337f76ab4522742f0dda14ee53
Alex Hoyau <a.hoyau@silexlabs.org>
2018-07-29 16:00:34 +0200

    fix email extraction regexp
    fixes #3
    fixes https://github.com/mautic/mautic/issues/3098

--- MauticHelper/output/firefox/assets/mautic.js
+++ MauticHelper/output/firefox/assets/mautic.js
@@ -111,7 +111,7 @@
               dest = to;
             } else {
                to = to.filter(String);
-               var re = /[a-z0-9\._%+-]+@[a-z0-9\.]+(\.[a-z0-9]{2,})?/img;
+               var re = /[a-z0-9\._%+-]+@[a-z0-9\._%-]+(\.[a-z0-9]{2,})?/img;
                dest = to.join(' ').match(re).join(';');
             }
             var qs = "from=" + encodeURIComponent(body_params.from ) + 
